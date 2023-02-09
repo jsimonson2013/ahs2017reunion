a9a0691460eeb213def4ea99f12a6c5e1e71a1f5
jsimonson2013 <jsimonson2013@gmail.com>
2023-02-09 07:18:04 -0700

    working on db integration

---
+++ go.mod
@@ -0,0 +1,5 @@
+module gioandjake.com/ahsreunion
+
+go 1.13
+
+require github.com/go-sql-driver/mysql v1.7.0
--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var DB *sql.DB
+
 func main() {
+	dbName := "reunion"
+	user := ""
+	pass := ""
+	protocol := "tcp"
+
+	DB, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s(127.0.0.1:3306)/%s", user, pass, protocol, dbName))
+	if err != nil {
+		panic(err)
+	}
+	defer DB.Close()
+
 	http.HandleFunc("/submit", submitForm)
 	http.HandleFunc("/rsvp", rsvp)
 	http.HandleFunc("/submit/rsvp", submitRSVP)
-	err := http.ListenAndServe(":3333", nil)
+	err = http.ListenAndServe(":3333", nil)
 	fmt.Println(err)
 }
 
@@ -32,6 +48,11 @@ func submitForm(w http.ResponseWriter, r *http.Request) {
 	email := r.URL.Query()["email"][0]
 	phone := r.URL.Query()["phone"][0]
 	method := r.URL.Query()["method"][0]
+
+	/* TODO
+	sql := "INSERT INTO cities(name, population) VALUES ('Moscow', 12506000)"
+	res, err := db.Exec(sql)
+	*/
 
 	f, err := os.OpenFile("records.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
@@ -57,6 +78,10 @@ func rsvp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	/* TODO
+	sql := "INSERT INTO cities(name, population) VALUES ('Moscow', 12506000)"
+	res, err := db.Exec(sql)
+	*/
 	// check that token exists in tokens db and is not expire
 	if token[0] != "1234" {
 		w.WriteHeader(403)
@@ -94,6 +119,11 @@ func submitRSVP(w http.ResponseWriter, r *http.Request) {
 	name := r.URL.Query()["name"][0]
 	attending := r.URL.Query()["attending"][0]
 	plusone := r.URL.Query()["plusone"][0]
+
+	/* TODO
+	sql := "INSERT INTO cities(name, population) VALUES ('Moscow', 12506000)"
+	res, err := db.Exec(sql)
+	*/
 
 	f, err := os.OpenFile("rsvps.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
